61a2d2cf2e1b19804e07e6e29aa7f962cdb2534e
Firsty Ukhti Molyndi <37396480+molzania@users.noreply.github.com>
2021-08-08 12:00:18 +0700

    Add files via upload

--- soal js 2.js
+++ soal js 2.js
@@ -5,6 +5,6 @@ if(a>b){
     console.log("Nilai A lebih besar dari nilai B");
 }else if(a<b){
     console.log("Nilai A lebih kecil dari nilai B");
-}else if(a=b){
+}else if(a==b){
     console.log("Nilai A sama dengan nilai B");
 }else{};
